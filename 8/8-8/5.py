1d31b5df7dc550a5603173b0fdf42eaa1138dcf5
ZhuchkaTrilesix <mrlololoshka94@gmail.com>
2024-09-06 17:35:36 +0300

    5

---
+++ 8/8-8/5.py
@@ -0,0 +1,5 @@
+files = ['python.png', 'qwerty.py', 'stepik.png', 'beegeek.org', 'windows.pnp', 'pen.txt', 'phone.py', 'book.txT',
+         'board.pNg', 'keyBoard.jpg', 'Python.PNg', 'apple.jpeg', 'png.png', 'input.tXt', 'split.pop', 'solution.Py',
+         'stepik.org', 'kotlin.ko', 'github.git']
+result = {c.lower() for c in files if c.lower().endswith('.png')}
+print(*sorted(result))
