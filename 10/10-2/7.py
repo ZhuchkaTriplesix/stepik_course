6b4d989b41f276b247e48164c635770fe1fec861
ZhuchkaTrilesix <mrlololoshka94@gmail.com>
2024-09-06 19:07:52 +0300

    7

---
+++ 10/10-2/7.py
@@ -0,0 +1,11 @@
+letters = [c for c in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789']
+morse = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---',
+         '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..', '-----', '.----', '..---',
+         '...--', '....-', '.....', '-....', '--...', '---..', '----.']
+
+work_map = dict(zip(letters, morse))
+
+input_string = input().upper()
+output_string = (work_map.get(letter) for letter in input_string if work_map.get(letter))
+
+print(' '.join(output_string))
