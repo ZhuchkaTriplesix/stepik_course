c02856e33482565c8ee11b3be04f11adb6bd800f
ZhuchkaTrilesix <mrlololoshka94@gmail.com>
2024-09-06 19:38:53 +0300

    3

---
+++ 10/10-5/3.py
@@ -0,0 +1,6 @@
+favorite_numbers = {'timur': 17, 'ruslan': 7, 'larisa': 19, 'roman': 123, 'rebecca': 293, 'ronald': 76, 'dorothy': 62,
+                    'harold': 36, 'matt': 314, 'kim': 451, 'rosaly': 18, 'rustam': 89, 'soltan': 111, 'amir': 654,
+                    'dima': 390, 'amiran': 777, 'geor': 999, 'sveta': 75, 'rita': 909, 'kirill': 404, 'olga': 271,
+                    'anna': 55, 'madlen': 876}
+
+result = {i: favorite_numbers[i] for i in favorite_numbers if int(favorite_numbers[i] / 10) in range(1, 10)}
