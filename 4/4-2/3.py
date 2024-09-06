b8c0c4a12439c01cff42d80fbf262449ee9fb8b2
ZhuchkaTrilesix <mrlololoshka94@gmail.com>
2024-09-06 14:26:15 +0300

    3

---
+++ 4/4-2/3.py
@@ -0,0 +1,7 @@
+list1 = [[1, 7, 8], [9, 7, 102], [6, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
+maximum = -1
+for i in list1:
+    for j in i:
+        if j > maximum:
+            maximum = j
+print(maximum)
