9a8d5f0a3ea4167d0c4cee2dccc180f5b7e4feac
ZhuchkaTrilesix <mrlololoshka94@gmail.com>
2024-09-06 14:23:20 +0300

    11

---
+++ 2/2-2/11.py
@@ -0,0 +1,7 @@
+word = input() + ' запретил букву'
+alpha = [chr(i) for i in range(1072, 1104)]
+
+for letter in alpha:
+    if letter in word:
+        print(word, letter)
+        word = word.replace(letter, '').replace('  ', ' ').strip()
