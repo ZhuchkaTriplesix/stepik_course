7c2a1c89790239af9776f0b6937d776687300719
ZhuchkaTrilesix <mrlololoshka94@gmail.com>
2024-09-06 19:12:17 +0300

    2

---
+++ 10/10-3/2.py
@@ -0,0 +1,6 @@
+dict1 = {'a': 100, 'z': 333, 'b': 200, 'c': 300, 'd': 45, 'e': 98, 't': 76, 'q': 34, 'f': 90, 'm': 230}
+dict2 = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z': 666}
+
+for key, value in dict2.items():
+    dict1[key] = dict1.setdefault(key, 0) + value
+result = dict1.copy()
