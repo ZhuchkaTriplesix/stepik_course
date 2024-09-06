83aa3e68b2474c4534776c34f82da56f1a1cb368
ZhuchkaTrilesix <mrlololoshka94@gmail.com>
2024-09-06 17:32:07 +0300

    2

---
+++ 8/8-8/2.py
@@ -0,0 +1,5 @@
+words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes', 'persimmon',
+         'tangerine', 'Watermelon', 'currant', 'Almond']
+
+myset = {i[0].lower() for i in words}
+print(*sorted(myset))
