c5de4634423e0b60f6cc03b7dccdd536819b3ef1
ZhuchkaTrilesix <mrlololoshka94@gmail.com>
2024-09-07 09:36:19 +0300

    ex

---
+++ 15/15-8/5.py
@@ -0,0 +1,7 @@
+words = ['beverage', 'monday', 'abroad', 'bias', 'abuse', 'abolish', 'abuse', 'abuse', 'bid', 'wednesday', 'able',
+         'betray', 'accident', 'abduct', 'bigot', 'bet', 'abandon', 'besides', 'access', 'friday', 'bestow', 'abound',
+         'absent', 'beware', 'abundant', 'abnormal', 'aboard', 'about', 'accelerate', 'abort', 'thursday', 'tuesday',
+         'sunday', 'berth', 'beyond', 'benevolent', 'abate', 'abide', 'bicycle', 'beside', 'accept', 'berry',
+         'bewilder', 'abrupt', 'saturday', 'accessory', 'absorb']
+
+print(*sorted(filter(lambda x: len(x) == 6, words)))
